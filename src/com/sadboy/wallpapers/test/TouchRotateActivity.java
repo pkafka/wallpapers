13fef3e523a6d485c7f46eeadcb5a48f12539d44
Patrick Kafka <patrick.kafka@gmail.com>
2010-08-04 00:44:08 -0600

    nfc

--- src/com/sadboy/wallpapers/test/TouchRotateActivity.java
+++ src/com/sadboy/wallpapers/test/TouchRotateActivity.java
@@ -139,60 +139,51 @@ class TouchSurfaceView extends GLSurfaceView {
         public void onDrawFrame(GL10 gl) {
 
         	double time = System.currentTimeMillis();
-        	mProjectile.updateLocationAndVelocity(time - mLastDraw);
+        	double elapsed = time - mLastDraw;
         	mLastDraw = time;
         	
-        	double z = mProjectile.getZ();
-        	if (mProjectile.getZ() >= 0.0)
-        	{
-        		String b = "Break";
-        	}
-
+        	mProjectile.updateLocationAndVelocity(0.03);
+        
             gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-    	    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-    	    
+
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadIdentity();
-            gl.glTranslatef(0, 0, -3.0f);
-            //gl.glTranslatef(0, 0, (float)mProjectile.getZ());
-            //gl.glRotatef(mAngleX, 0, 1, 0);
-            //gl.glRotatef(mAngleY, 1, 0, 0);
+            //gl.glTranslatef(0, 0, -3.0f);
+            gl.glTranslatef(0, 0, (float)mProjectile.getZ());
+            gl.glRotatef(mAngleX, 0, 1, 0);
+            gl.glRotatef(mAngleY, 1, 0, 0);
+
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
 
             mCube.draw(gl);
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
-             _width = width;
-     	    _height = height;
-
+        	/*
+        	gl.glViewport(0, 0, width, height);
      	    gl.glMatrixMode(GL10.GL_PROJECTION);
-    	    float ratio = _width / _height;
-    	    GLU.gluPerspective(gl, 45.0f, ratio, 1.0f, 20.0f); 
+            gl.glLoadIdentity();
+    	    //GLU.gluPerspective(gl, 45.0f, ratio, 1.0f, 20.0f); 
+    	     *
+    	     */
+
+        	gl.glViewport(0, 0, width, height);
+        	
+    	    float ratio = (float) width / height;
+            gl.glMatrixMode(GL10.GL_PROJECTION);
+            gl.glLoadIdentity();
+            gl.glFrustumf(-ratio, ratio, -1, 1, 1, 50);
         }
 
-        
-
-        private float _width = 320f;
-        private float _height = 480f;
-        
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+             gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                     GL10.GL_FASTEST);
 
-        	 gl.glMatrixMode(GL10.GL_PROJECTION);
-     	    float ratio = _width / _height;
-     	    GLU.gluPerspective(gl, 45.0f, ratio, 1.0f, 20.0f); 
-    	    
-    	    gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                    GL10.GL_FASTEST);
-    	    gl.glEnable(GL10.GL_DEPTH_TEST);
-    	 
-    	    // clipping wall color
-    	    gl.glClearColor(0f, 0.5f, 0f, 1.0f);
-    	 
-    	    gl.glEnable(GL10.GL_CULL_FACE);
-    	    gl.glFrontFace(GL10.GL_CCW);
-    	    gl.glCullFace(GL10.GL_FRONT);
-    	    gl.glShadeModel(GL10.GL_SMOOTH);
+             gl.glClearColor(1,1,1,1);
+             gl.glEnable(GL10.GL_CULL_FACE);
+             gl.glShadeModel(GL10.GL_SMOOTH);
+             gl.glEnable(GL10.GL_DEPTH_TEST);
         }
     }
 
