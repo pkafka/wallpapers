f842abe1ae0c3ff44547e0a29c47780bd7975bef
Patrick Kafka <patrick.kafka@gmail.com>
2010-08-03 23:21:48 -0600

    nfc

--- src/com/sadboy/wallpapers/test/TouchRotateActivity.java
+++ src/com/sadboy/wallpapers/test/TouchRotateActivity.java
@@ -19,9 +19,12 @@ package com.sadboy.wallpapers.test;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import com.sadboy.wallpapers.physics.SimpleProjectile;
+
 import android.app.Activity;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.view.MotionEvent;
@@ -72,6 +75,14 @@ public class TouchRotateActivity extends Activity {
  */
 class TouchSurfaceView extends GLSurfaceView {
 
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private final float TRACKBALL_SCALE_FACTOR = 36.0f;
+    private CubeRenderer mRenderer;
+    private float mPreviousX;
+    private float mPreviousY;
+    
+    
     public TouchSurfaceView(Context context) {
         super(context);
         mRenderer = new CubeRenderer();
@@ -109,92 +120,82 @@ class TouchSurfaceView extends GLSurfaceView {
      * Render a cube.
      */
     private class CubeRenderer implements GLSurfaceView.Renderer {
+    	
+
+        private Cube mCube;
+        public float mAngleX;
+        public float mAngleY;
+        
+        SimpleProjectile mProjectile;
+        double mLastDraw;
+        
         public CubeRenderer() {
             mCube = new Cube();
             mLastDraw = System.currentTimeMillis();
-            mGaps = new long[100];
+            mProjectile = new SimpleProjectile(
+        		   0.0, 0.0, 0.0, 0.0, 35.0, 0.0, mLastDraw);
         }
-
-        long mLastDraw;
-        long[] mGaps;
-        int count = -1;
         
         public void onDrawFrame(GL10 gl) {
 
-        	count++;
+        	double time = System.currentTimeMillis();
+        	mProjectile.updateLocationAndVelocity(time - mLastDraw);
+        	mLastDraw = time;
         	
-        	if (count < 100){
-	        	mGaps[count] = System.currentTimeMillis() - mLastDraw;
-	            mLastDraw = System.currentTimeMillis();;
+        	double z = mProjectile.getZ();
+        	if (mProjectile.getZ() >= 0.0)
+        	{
+        		String b = "Break";
         	}
-        	else{
-        		String s = "br";
-        	}
-        	
+
             gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-
-            /*
-             * Now we're ready to draw some 3D objects
-             */
-
+    	    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+    	    
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadIdentity();
             gl.glTranslatef(0, 0, -3.0f);
-            gl.glRotatef(mAngleX, 0, 1, 0);
-            gl.glRotatef(mAngleY, 1, 0, 0);
-
-            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+            //gl.glTranslatef(0, 0, (float)mProjectile.getZ());
+            //gl.glRotatef(mAngleX, 0, 1, 0);
+            //gl.glRotatef(mAngleY, 1, 0, 0);
 
             mCube.draw(gl);
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
-             gl.glViewport(0, 0, width, height);
+             _width = width;
+     	    _height = height;
 
-             /*
-              * Set our projection matrix. This doesn't have to be done
-              * each time we draw, but usually a new projection needs to
-              * be set when the viewport is resized.
-              */
-
-             float ratio = (float) width / height;
-             gl.glMatrixMode(GL10.GL_PROJECTION);
-             gl.glLoadIdentity();
-             gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+     	    gl.glMatrixMode(GL10.GL_PROJECTION);
+    	    float ratio = _width / _height;
+    	    GLU.gluPerspective(gl, 45.0f, ratio, 1.0f, 20.0f); 
         }
 
+        
+
+        private float _width = 320f;
+        private float _height = 480f;
+        
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            /*
-             * By default, OpenGL enables features that improve quality
-             * but reduce performance. One might want to tweak that
-             * especially on software renderer.
-             */
-            gl.glDisable(GL10.GL_DITHER);
 
-            /*
-             * Some one-time OpenGL initialization can be made here
-             * probably based on features of this particular context
-             */
-             gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                     GL10.GL_FASTEST);
-
-
-             gl.glClearColor(1,1,1,1);
-             gl.glEnable(GL10.GL_CULL_FACE);
-             gl.glShadeModel(GL10.GL_SMOOTH);
-             gl.glEnable(GL10.GL_DEPTH_TEST);
+        	 gl.glMatrixMode(GL10.GL_PROJECTION);
+     	    float ratio = _width / _height;
+     	    GLU.gluPerspective(gl, 45.0f, ratio, 1.0f, 20.0f); 
+    	    
+    	    gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+                    GL10.GL_FASTEST);
+    	    gl.glEnable(GL10.GL_DEPTH_TEST);
+    	 
+    	    // clipping wall color
+    	    gl.glClearColor(0f, 0.5f, 0f, 1.0f);
+    	 
+    	    gl.glEnable(GL10.GL_CULL_FACE);
+    	    gl.glFrontFace(GL10.GL_CCW);
+    	    gl.glCullFace(GL10.GL_FRONT);
+    	    gl.glShadeModel(GL10.GL_SMOOTH);
         }
-        private Cube mCube;
-        public float mAngleX;
-        public float mAngleY;
     }
 
-    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
-    private final float TRACKBALL_SCALE_FACTOR = 36.0f;
-    private CubeRenderer mRenderer;
-    private float mPreviousX;
-    private float mPreviousY;
     
 
 }
