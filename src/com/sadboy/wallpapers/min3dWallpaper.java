cadb4a977da8da1caa90a3610fd9d38eca5c2882
Patrick Kafka <patrick.kafka@gmail.com>
2010-08-24 21:45:25 -0600

    experimental obj import and render

--- src/com/sadboy/wallpapers/min3dWallpaper.java
+++ src/com/sadboy/wallpapers/min3dWallpaper.java
@@ -11,6 +11,8 @@ import min3d.core.Scene;
 import min3d.interfaces.ISceneController;
 import min3d.objectPrimitives.Box;
 import min3d.objectPrimitives.Sphere;
+import min3d.parser.IParser;
+import min3d.parser.Parser;
 import min3d.vos.Light;
 import min3d.vos.LightType;
 
@@ -54,18 +56,7 @@ public class Min3dWallpaper extends GLWallpaperService {
     	min3d.core.Renderer _renderer;
     	public Scene _scene;
     	
-    	private final float DEGREE = (float)(Math.PI/180);
-
-    	Object3dContainer _sphere;
-    	
-    	Light _lightRed;
-    	Light _lightGreen;
-    	Light _lightBlue;
-    	Object3dContainer _boxRed;
-    	Object3dContainer _boxGreen;
-    	Object3dContainer _boxBlue;
-
-    	int _count = 0;
+    	private Object3dContainer objModel;
     	
         public Min3dRenderer(Context c) {
 
@@ -82,41 +73,9 @@ public class Min3dWallpaper extends GLWallpaperService {
         
         public void onDrawFrame(GL10 gl) {
 
-        	// Set lights' position
+    		objModel.rotation().x++;
+    		objModel.rotation().z++;
     		
-    		_lightRed.position.setAll(0, +0.9f, 2.5f);
-    		float x = (float)(Math.sin(_count%360 * DEGREE) * -1.5f);
-    		float z = (float)(Math.cos(_count%360 * DEGREE) * 2.25f);
-    		_lightGreen.position.setAll(x, 0, z);
-    		_lightBlue.position.setAll(0, -0.9f, 2.5f);
-    		
-    		// Sync boxes' position with lights
-    		
-    		_boxRed.position().setAllFrom(_lightRed.position.toNumber3d());
-    		_boxGreen.position().setAllFrom(_lightGreen.position.toNumber3d());
-    		_boxBlue.position().setAllFrom(_lightBlue.position.toNumber3d());
-
-    		// Red light pulses by changing diffuse red property
-    		
-    		short mag = (short)(255 - (_count % 60) * (255/60));
-    		_lightRed.diffuse.r(mag);
-    		_boxRed.defaultColor().r = mag;
-
-    		// Green light blinks on and off using isVisible() property
-    		
-    		if (_count % 30 == 0) {
-    			_lightGreen.isVisible(true);
-    			_boxGreen.isVisible(true);
-    		}
-    		else if (_count % 30 == 20) {
-    			_lightGreen.isVisible(false);
-    			_boxGreen.isVisible(false);
-    		}
-
-    		_sphere.rotation().y += 1;
-    		_count++;
-
-        	
         	_renderer.onDrawFrame(gl);
         }
 
@@ -132,55 +91,15 @@ public class Min3dWallpaper extends GLWallpaperService {
         
         public void initScene() 
     	{
-    		_scene.camera().position.setAll(0,0,5);
-
-    		// Add three lights
+    		_scene.lights().add(new Light());
     		
-    		_lightRed = new Light();
-    		_lightRed.ambient.setAll(0x88110000);
-    		_lightRed.diffuse.setAll(0xffff0000);
-    		_lightRed.type(LightType.POSITIONAL); // looks nicer, especially with multiple lights interacting with each other
-    		_scene.lights().add(_lightRed);
+    		IParser parser = Parser.createParser(Parser.Type.OBJ,
+    				getResources(), "com.sadboy.wallpapers:raw/die_obj", false);
+    		parser.parse();
 
-    		_lightGreen = new Light();
-    		_lightGreen.ambient.setAll(0x88001100);
-    		_lightGreen.diffuse.setAll(0xff00ff00);
-    		_lightGreen.type(LightType.POSITIONAL);
-    		_scene.lights().add(_lightGreen);
-
-    		_lightBlue = new Light();
-    		_lightBlue.ambient.setAll(0x88000011);
-    		_lightBlue.diffuse.setAll(0xff0000ff);
-    		_lightBlue.type(LightType.POSITIONAL); 
-    		_scene.lights().add(_lightBlue);
-
-    		// The objects that we'll light up
-    		
-    		_sphere = new Sphere(1.0f, 20, 15);
-    		_sphere.vertexColorsEnabled(false);
-    		_scene.addChild(_sphere);
-    		
-    		// Boxes, displayed just as a visualization aid
-    		
-    		_boxRed = new Box(0.05f,0.05f,0.05f);
-    		_boxRed.normalsEnabled(false);
-    		_boxRed.vertexColorsEnabled(false);
-    		_boxRed.defaultColor().setAll(0xaaff0000);
-    		_scene.addChild(_boxRed);
-    		
-    		_boxGreen = new Box(0.07f,0.07f,0.07f);
-    		_boxGreen.normalsEnabled(false);
-    		_boxGreen.vertexColorsEnabled(false);
-    		_boxGreen.defaultColor().setAll(0xaa00ff00);
-    		_scene.addChild(_boxGreen);
-
-    		_boxBlue = new Box(0.05f,0.05f,0.05f);
-    		_boxBlue.normalsEnabled(false);
-    		_boxBlue.vertexColorsEnabled(false);
-    		_boxBlue.defaultColor().setAll(0xaa0000ff);
-    		_scene.addChild(_boxBlue);
-
-    		_count = 0;
+    		objModel = parser.getParsedObject();
+    		objModel.scale().x = objModel.scale().y = objModel.scale().z = .7f;
+    		_scene.addChild(objModel);
     	}
         
     
